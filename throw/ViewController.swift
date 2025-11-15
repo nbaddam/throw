41bea67bb0894a8ea09099e1b9c7850e6121a001
Nitya Reddy Baddam <nityabaddam@gmail.com>
2025-11-15 16:13:38 -0500

    initial commit with new project

---
+++ throw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  throw
+//
+//  Created by Nitya Baddam on 11/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
